01d52b0678727a6407f86caddd8c2a3baaab3865
michaelhos412 <u3560321@connect.hku.hk>
2022-02-07 21:27:37 +0800

    added obtaining frame number functions

--- Assets/Scripts/SkeletonTester.cs
+++ Assets/Scripts/SkeletonTester.cs
@@ -24,40 +24,28 @@ public class SkeletonTester : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
 
-            for (int i = 0; i < m_skeletonDef.Bones.Count; ++i)
-            {
-                Int32 boneId = m_skeletonDef.Bones[i].Id;
-                Debug.Log("bone name: " + m_skeletonDef.Bones[i].Name + " " + boneId);
-                OptitrackPose bonePose;
-                OptitrackPose bonePose2;
-                bonePose = bot1.saveLoadManager.skeletonStateBuffer.stateList[0].BonePoses[boneId];
-                bonePose2 = bot1.saveLoadManager.skeletonStateBuffer.stateList[0].LocalBonePoses[boneId];
-                Debug.Log("0: " + bot1.saveLoadManager.skeletonStateBuffer.stateList[0] + " " + i);
-                Vector3 globalPos = evaluation.GetGlobalValue(bot1.saveLoadManager.skeletonStateBuffer.stateList[0], m_skeletonDef, boneId, new Vector3());
-                Debug.Log("1: bonePose" + bonePose.Position.x + " " + bonePose.Position.y + " " + bonePose.Position.z);
-                Debug.Log("2: " + globalPos.x + " " + globalPos.y + " " + globalPos.z);
-                Debug.Log("3: localBonePose" + bonePose2.Position.x + " " + bonePose2.Position.y + " " + bonePose2.Position.z);
-                Debug.Log(bot1.saveLoadManager.skeletonStateBuffer.stateList[0].BonePoses);
-            }
+            // for (int i = 0; i < m_skeletonDef.Bones.Count; ++i)
+            // {
+            //     Int32 boneId = m_skeletonDef.Bones[i].Id;
+            //     Debug.Log("bone name: " + m_skeletonDef.Bones[i].Name + " " + boneId);
+            //     // OptitrackPose bonePose;
+            //     // OptitrackPose bonePose2;
+            //     // bonePose = bot1.saveLoadManager.skeletonStateBuffer.stateList[0].BonePoses[boneId];
+            //     // bonePose2 = bot1.saveLoadManager.skeletonStateBuffer.stateList[0].LocalBonePoses[boneId];
+            //     // Debug.Log("0: " + bot1.saveLoadManager.skeletonStateBuffer.stateList[0] + " " + i);
+            //     // Vector3 globalPos = evaluation.GetGlobalValue(bot1.saveLoadManager.skeletonStateBuffer.stateList[0], m_skeletonDef, boneId, new Vector3());
+            //     // Debug.Log("1: bonePose" + bonePose.Position.x + " " + bonePose.Position.y + " " + bonePose.Position.z);
+            //     // Debug.Log("2: " + globalPos.x + " " + globalPos.y + " " + globalPos.z);
+            //     // Debug.Log("3: localBonePose" + bonePose2.Position.x + " " + bonePose2.Position.y + " " + bonePose2.Position.z);
+            //     // Debug.Log(bot1.saveLoadManager.skeletonStateBuffer.stateList[0].BonePoses);
+            // }
 
             //bot1.SetSkeletonFrame(600);
 
 
-                Debug.Log(evaluation.GetHighestBodyPart(bot1.saveLoadManager.skeletonStateBuffer.stateList, 9, m_skeletonDef ));
-
-                //Debug.Log(evaluation.GetHighestBodyPart2(bot1.saveLoadManager.skeletonStateBuffer.stateList, m_skeletonDef.BoneIdToParentIdMap, boneId));
-                // bot1.SetSkeletonFrame(0);
-                //bot1.SetSkeletonFrame(evaluation.GetHighestBodyPart(bot1.saveLoadManager.skeletonStateBuffer.stateList,boneId));
-                //bot2.SetSkeletonFrame(52);
-                //OptitrackPose pose;
-                //Debug.Log(bot1.saveLoadManager.skeletonStateBuffer.stateList[0].BonePoses.TryGetValue();
-                //Debug.Log(bot1.saveLoadManager.skeletonStateBuffer.stateList[0].LocalBonePoses);
-                //Vector3 pos = evaluation.GetGlobalValue(bot1.saveLoadManager.skeletonStateBuffer.stateList[0], m_skeletonDef, boneId);
-                //Debug.Log("global pos : " + pos.x + " " + pos.y + " " + pos.z);
-
-                //Debug.Log(bot1.saveLoadManager.skeletonStateBuffer.stateList[632].BonePoses.values[12].Position);
-                //bool foundPose = bot1.saveLoadManager.skeletonStateBuffer.stateList[632].BonePoses.TryGetValue(13, out pose);
-                //Debug.Log(pose.Position);
+                // Debug.Log(evaluation.GetHighestBodyPart(bot1.saveLoadManager.skeletonStateBuffer.stateList, 8, m_skeletonDef ));
+                bot1.SetSkeletonFrame(evaluation.GetHighestBodyPart(bot1.saveLoadManager.skeletonStateBuffer.stateList, 13));
+                Debug.Log(evaluation.GetFollowThrough(bot1.saveLoadManager.skeletonStateBuffer.stateList));
         }
     }
 }
